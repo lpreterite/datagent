6ebf0d102793f8f944567e3a8c28248e9dacf023
Pakcy-Tang <lpreterite@126.com>
2019-11-04 17:52:58 +0800

    update index export

--- src/index.js
+++ src/index.js
@@ -5,7 +5,8 @@ import { default as contact, constructor as Contact } from "./contact"
 import { default as schema, constructor as Schema } from "./schema"
 import { default as agent, constructor as Agent } from "./agent"
 import { default as context, constructor as Context } from "./context"
-const classes = { Model, Contact, Schema, Agent, Context }
+import { default as remote, constructor as Remote } from "./remote"
+const classes = { Model, Contact, Schema, Agent, Context, Remote }
 const constructors = classes
-export { hooks, utils, model, contact, schema, agent, context, classes, constructors }
-export default { utils, hooks, model, contact, schema, agent, context, classes, constructors }
+export { hooks, utils, model, contact, schema, agent, context, remote, classes, constructors }
+export default { utils, hooks, model, contact, schema, agent, context, remote, classes, constructors }
