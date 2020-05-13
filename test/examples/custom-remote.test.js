0f9ca49242715acce93365ef68827d26b39ddb9b
Packy <lpreterite@126.com>
2020-05-13 14:33:57 +0800

    fix timeout setting in custom remote test

--- test/examples/custom-remote.test.js
+++ test/examples/custom-remote.test.js
@@ -13,7 +13,6 @@ const handle = res => {
 describe('Custom Remote Test', function(){
     let contact, remote, post;
     before(function (){
-        this.timeout(3000)
         contact = datagent.contact(
             //remote的设定
             {
@@ -29,26 +28,31 @@ describe('Custom Remote Test', function(){
         post = null
     })
     it('发送GET请求', async function(){
+        this.timeout(5000)
         let err, result;
         [err, result] = await contact.remote().get('/todos/3').then(handle);
         assert.propertyVal(result, 'title', 'fugiat veniam minus')
     })
     it('发送POST请求', async function(){
+        this.timeout(5000)
         let err, result;
         [err, result] = await contact.remote().post('/todos', { title: "do something", completed: false, userId: 1 }).then(handle);
         assert.property(result, "id", "应当返回id字段");
     })
     it('发送PUT请求', async function(){
+        this.timeout(5000)
         let err, result;
         [err, result] = await contact.remote().put('/todos/3', { id: 3, title: "fugiat veniam minus", completed: true, userId: 1 }).then(handle);
         assert.propertyVal(result, "completed", true);
     })
     it('发送PATCH请求', async function(){
+        this.timeout(5000)
         let err, result;
         [err, result] = await contact.remote().patch('/todos/3', { id: 3, completed: true }).then(handle);
         assert.propertyVal(result, "completed", true);
     })
     it('发送DELETE请求', async function(){
+        this.timeout(5000)
         const res = await contact.remote().delete('/todos/3');
         assert.propertyVal(res, "status", 200);
     })
